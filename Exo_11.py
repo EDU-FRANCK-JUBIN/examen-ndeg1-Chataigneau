c53939841f3b3b3a9206253f32b8a60a091354b3
Nico <skywosh@gmail.com>
2019-11-08 12:10:59 +0100

    Create Exo_11.py

---
+++ Exo_11.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 11:32:49 2019
+
+@author: Nicolas Chataigneau
+"""
+
+import turtle as tu
+
+tu.reset()
+
+tu.rt(50)
+tu.fd(50)
+tu.fd(50)
+tu.lt(50)
+tu.fd(50)
+tu.speed(50)
