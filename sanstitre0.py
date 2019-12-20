32c9469b9cf01f7294f8479e172b442c5d5cf694
Nico <skywosh@gmail.com>
2019-12-20 10:43:24 +0100

    Create sanstitre0.py

---
+++ sanstitre0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 12:26:26 2019
+
+@author: Nicolas Chataigneau
+"""
+
+from pydatalog import pydatalog as pd
+
+pd.create_terms("person, specialty, contract, division, all_persons") 
